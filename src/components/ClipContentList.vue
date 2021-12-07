00ca08e9bb1510761dfc50f56dd55a242ac815cc
Mingzz <184653774@qq.com>
2021-12-07 18:19:45 +0800

    [Feat] 监听粘贴事件, 文件上传COS

--- src/components/ClipContentList.vue
+++ src/components/ClipContentList.vue
@@ -10,46 +10,44 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, toRefs } from "vue";
+import { defineComponent, reactive, toRefs, onMounted } from "vue";
 import { ipcRenderer } from "electron";
-import { Config } from "../config";
+import { Config, CosInstance } from "../config";
 import Centrifuge from "centrifuge";
-
 
 export default defineComponent({
   name: "ClipContentList",
   setup() {
     const data = reactive({
       clipContentList: [] as string[],
-      currentUser: 'test1234'
+      currentUser: "test1234",
     });
 
-    const centrifuge = new Centrifuge(
-      Config.wsServerURL
-    );
+    const centrifuge = new Centrifuge(Config.wsServerURL);
     centrifuge.setConnectData({
       user_id: data.currentUser,
       new_enter_id: 1789317,
       token: Config.wsToken,
     });
 
+    function addToClipContentList(title: string): void {
+      if (!data.clipContentList.includes(title)) {
+        data.clipContentList.push(title);
+      }
+    }
+
+    // 订阅ws
     centrifuge.subscribe(Config.wsChannel, function (message) {
       const remoteData = message.data;
       const sender = remoteData.sender;
       const content = remoteData.content;
       if (sender === data.currentUser) {
-        const clipText = content.text;
-        if (!data.clipContentList.includes(clipText)) {
-          data.clipContentList.push(content.text);
-        }
+        addToClipContentList(content.text);
       }
     });
 
-    ipcRenderer.on("pageData", function (event, message) {
-      if (!data.clipContentList.includes(message.text)) {
-        data.clipContentList.push(message.text);
-      }
-      centrifuge
+    function sendMessage(message: unknown): Promise<unknown> {
+      return centrifuge
         .namedRPC("send_message", {
           channel: Config.wsChannel,
           client_ts: `${Date.now()}`,
@@ -63,6 +61,77 @@ export default defineComponent({
             console.log("rpc error", err);
           }
         );
+    }
+
+    function clipboardListAddItem(title: string, message: unknown) {
+      addToClipContentList(title);
+      sendMessage(message);
+    }
+
+    function putObject(fileObj: File) {
+      CosInstance.putObject(
+        {
+          Bucket: "file-transfer-1252809026" /* 必须 */,
+          Region: "ap-guangzhou" /* 存储桶所在地域，必须字段 */,
+          Key: fileObj.name /* 必须 */,
+          StorageClass: "STANDARD",
+          Body: fileObj, // 上传文件对象
+          // onProgress: (progressData) => {
+          //   if (progressData.percent === 1) {
+          //     // 上传完成
+          //   }
+          // },
+        },
+        (err: unknown, data: any) => {
+          if (!err) {
+            let resUrl = data.Location;
+            console.log("resUrl", resUrl);
+            clipboardListAddItem(resUrl, { text: resUrl });
+          }
+        }
+      );
+    }
+
+    onMounted(() => {
+      document.addEventListener(
+        "paste",
+        (event) => {
+          console.group("paste");
+          console.log("event", event);
+          console.log("clipboardData", event.clipboardData);
+          console.log("items", event.clipboardData?.items);
+          console.log("types", event.clipboardData?.types);
+          console.log("fileList", event.clipboardData?.files);
+          console.log("getData", event.clipboardData?.getData("Files"));
+
+          let kinds: string[] = [];
+          for (let item of event.clipboardData?.items || []) {
+            kinds.push(item.kind);
+          }
+          console.log("kinds", kinds);
+
+          console.groupEnd();
+
+          for (let entity of event.clipboardData?.items || []) {
+            if (entity.kind === "string") {
+              entity.getAsString((message) => {
+                clipboardListAddItem(message, { text: message });
+              });
+            } else if (entity.kind === "file") {
+              let file = entity.getAsFile();
+              if (file) {
+                putObject(file);
+              }
+            }
+          }
+        },
+        { capture: false }
+      );
+    });
+
+    ipcRenderer.on("pageData", function (event, message) {
+      console.log("pageData", message);
+      clipboardListAddItem(message.text, message);
     });
 
     centrifuge.connect();
