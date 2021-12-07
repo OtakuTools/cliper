eac61316e716f7a7777bc5db6d2c1ece71e09593
Mingzz <184653774@qq.com>
2021-12-07 16:00:21 +0800

    [Feat] 引入axios和COS

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,34 @@
-export const Config = {
-  wsServerURL: '',
-  wsChannel: '',
-  wsToken: '',
+import axios from "axios";
 
-  sendPassword: '',
-  receivePassword: '',
-}
+export const Config = {
+  wsServerURL: "wss://iclass-saas.api.qcloud.com/connection/websocket",
+  wsChannel: "public:news",
+  wsToken: "553b9d161be673699d90e8059944489c",
+
+  sendPassword: "",
+  receivePassword: "",
+};
+
+export const CosInstance = new window.COS({
+  async getAuthorization(options: unknown, callback: (arg0: unknown) => void) {
+    // 异步获取临时密钥
+    const res = await axios({
+      method: "post",
+      url: "https://env-nuxt-ssr-7ga885o173201dc0-1252809026.ap-shanghai.app.tcloudbase.com/transfer_cos",
+      responseType: "json",
+      data: {
+        cmd: "getToken",
+      },
+    });
+    const { data } = res;
+    const credentials = data && data.credentials;
+    if (!data || !credentials) return console.error("credentials invalid");
+    callback({
+      TmpSecretId: credentials.tmpSecretId,
+      TmpSecretKey: credentials.tmpSecretKey,
+      XCosSecurityToken: credentials.sessionToken,
+      StartTime: data.startTime,
+      ExpiredTime: data.expiredTime,
+    });
+  },
+});
