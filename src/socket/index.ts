27565e7a7b294f8adb1b1772f5232cd91e77d84d
Mingzz <184653774@qq.com>
2021-12-24 02:08:02 +0800

    [Feat] 网络断开提醒

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,3 +1,5 @@
+import { isOnline } from "../store"
+
 export interface SocketMessage {
   name: string
   type: "url" | "text"
@@ -10,6 +12,7 @@ export default class Socket {
 
   msgCallback: (data: SocketMessage[]) => void
   socket: WebSocket
+  alive = true
 
   constructor(channelId: string, msgCallback: (data: SocketMessage[]) => void) {
     this.socket = new WebSocket(`ws://service-7xa83x7r-1259648581.sh.apigw.tencentcs.com:80/websocket?channelID=` + channelId);
@@ -17,11 +20,11 @@ export default class Socket {
     this.initEvent();
   }
 
-  socketSend(message: SocketMessage[]) {
-    return this.socket.send(JSON.stringify(message))
+  socketSend(message: SocketMessage[]): void {
+    this.alive && this.socket.send(JSON.stringify(message))
   }
 
-  initEvent() {
+  initEvent(): void {
     this.socket.addEventListener('message', ({ data: data }) => {
       try {
         this.msgCallback(JSON.parse(data) as SocketMessage[]);
@@ -30,20 +33,33 @@ export default class Socket {
       }
     });
 
-    this.socket.addEventListener('close', (e) => { console.log('close', e) })
-    this.socket.addEventListener('error', (e) => { console.log('error', e) })
-    this.socket.addEventListener('disconnect', (e) => { console.log('disconnect', e) })
+    this.socket.addEventListener('close', (e) => {
+      console.log('close', e);
+      this.setOffline();
+    })
+    this.socket.addEventListener('error', (e) => {
+      console.log('error', e);
+    })
+    this.socket.addEventListener('disconnect', (e) => {
+      console.log('disconnect', e);
+      this.setOffline();
+    })
 
     this.keepAlive();
   }
 
-  keepAlive() {
-    setInterval(() => {
-      this.socketSend([]);
-    }, 10000);
+  setOffline(): void {
+    this.alive = false;
+    isOnline.value = false;
   }
 
-  close() {
-    this.socket.close()
+  keepAlive(): void {
+    setInterval(() => {
+      this.socketSend([]);
+    }, 1000);
+  }
+
+  close(): void {
+    this.alive && this.socket.close()
   }
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,6 @@
 import { createStore } from "vuex";
+import { ref, watchEffect } from 'vue'
+import { ElMessage, MessageHandle } from 'element-plus'
 
 export default createStore({
   state: {},
@@ -6,3 +8,28 @@ export default createStore({
   actions: {},
   modules: {},
 });
+
+// 离线(含ws断开)提醒逻辑
+// FIXME: 由于ws不是单例, 如果部分ws断开就会触发提醒, 如果只有部分ws重连成功(未实现), 则状态错误
+/** 是否在线 */
+export const isOnline = ref(navigator.onLine)
+window.addEventListener('online', () => {
+  isOnline.value = true
+})
+window.addEventListener('offline', () => {
+  isOnline.value = false
+})
+let message: MessageHandle | null = null
+watchEffect(() => {
+  if(isOnline.value){
+    message?.close()
+    message = null
+  } else {
+    message = ElMessage({
+      message: 'You are offline',
+      type: 'warning',
+      showClose: true,
+      duration: 0
+    })
+  }
+})
