1663290396674c542d63a905f6143434d1551fe7
Mingzz <184653774@qq.com>
2022-01-08 01:46:24 +0800

    [Feat]: chrome extension bridge implement

--- src/extentsions/background/bridge.ts
+++ src/extentsions/background/bridge.ts
@@ -1,10 +1,10 @@
-import { isElectron, EventName, EVENT, isExtension } from '../constant';
-import axios from 'axios';
+import { EventName } from '@/constant';
 
 /** bus事件回调 */
 type EventCallback = (event: unknown, ...arg: any[]) => void;
 
 /** bus的简单实现 */
+// TODO: 复用@/store/bridge中的bus
 class Bus {
   protected handlers: { [event in EventName]?: EventCallback[] } = {};
 
@@ -70,72 +70,4 @@ class ExtensionBridge extends Bus implements Bus {
   }
 }
 
-// 纯js下载文件
-function downloadFile(file: any): Promise<void> {
-  return axios({
-    url: file.downloadUrl,
-    method: 'get',
-    responseType: 'blob'
-  }).then((res) => {
-    const link = document.createElement('a');
-    const blob: Blob = res.data;
-    link.setAttribute('href', window.URL.createObjectURL(blob));
-    link.setAttribute('download', file.name);
-    link.click();
-  })
-}
-
-// web端bridge实现
-function initLocalBridge(bridge: Bus): Bus {
-  // download事件
-  bridge.on(EVENT.DOWNLOAD, (evt, payload) => {
-    try {
-      const file = JSON.parse(payload);
-      downloadFile(file);
-    } catch (err) {
-      console.error('下载文件失败', err);
-    }
-  })
-
-  return bridge;
-}
-
-// extension bridge
-function initExtensionBridge(bridge: ExtensionBridge): ExtensionBridge {
-  // background回传的download事件
-  bridge.on(EVENT.DOWNLOAD, (evt, payload) => {
-    console.warn('extension download fallback');
-    try {
-      const file = JSON.parse(payload);
-      // no access to extension downloads API, fallback to axios download
-      downloadFile(file);
-    } catch (err) {
-      console.error('下载文件失败', err);
-    }
-  })
-
-  return bridge;
-}
-
-function getBridge() {
-  if (isElectron) {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { ipcRenderer } = require('electron');
-    return ipcRenderer;
-  } else if (isExtension) { 
-    const bridge = new ExtensionBridge();
-    initExtensionBridge(bridge);
-    return bridge;
-   } else {
-    const bridge = new Bus();
-    initLocalBridge(bridge);
-    return bridge;
-  }
-}
-
-/** 跨进程通信 */
-export const bridge = getBridge();
-
-/** 页面bus */
-export const bus = new Bus();
-
+export default new ExtensionBridge();
--- src/extentsions/background/index.ts
+++ src/extentsions/background/index.ts
@@ -1,1 +1,22 @@
-console.log('background...'); // eslint-disable-line 
+import bridge from './bridge';
+import { EVENT } from '@/constant';
+
+// download事件
+bridge.on(EVENT.DOWNLOAD, (evt, payload) => {
+  try {
+    const file = JSON.parse(payload);
+    if (window?.chrome?.downloads) {
+      // use extension downloads API
+      window.chrome.downloads.download({
+        filename: file.name as string,
+        url: file.downloadUrl,
+        conflictAction: 'prompt', // 文件重名的策略 // prompt 提示用户选择, uniquify 自动重命名, overwrite 覆盖
+      })
+    } else {
+      // no access to extension downloads API, send back to popup page to use axios download
+      bridge.send(EVENT.DOWNLOAD, payload);
+    }
+  } catch (err) {
+    console.error('下载文件失败', err);
+  }
+})
