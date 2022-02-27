ca22ee1ca5e52dbcc8dc5e6c61b0f598d79a17ce
ZTao-z <1124693098@qq.com>
2022-02-27 22:07:11 +0800

    [Opt] 调整下载按钮

--- src/components/RecvListComponent.vue
+++ src/components/RecvListComponent.vue
@@ -1,10 +1,17 @@
 <template>
   <card>
-    <div class="data_container" v-for="data in recvFileList" v-bind:key="data.name" @click="downloadData(data)">
-      <div class="data_text">{{ data.name }}</div>
-      <div class="data_info">
-        <span class="data_size">大小：{{ (data.size / 1000).toFixed(2) + 'KB' }}</span>
-        <span class="data_time">时间：{{ formatDate(data.timestamp) }}</span>
+    <div class="data_container" v-for="data in recvFileList" v-bind:key="data.name">
+      <div class="content_box">
+        <div class="data_text">{{ data.name }}</div>
+        <div class="data_info">
+          <span class="data_size">大小：{{ (data.size / 1000).toFixed(2) + 'KB' }}</span>
+          <span class="data_time">时间：{{ formatDate(data.timestamp) }}</span>
+        </div>
+      </div>
+      <div class="button_box">
+        <el-button v-if="data.type === 'url'" class="buttons" :icon="Download" circle @click="downloadData(data)"></el-button>
+        <el-button v-else class="buttons" :icon="CopyDocument"  circle @click="downloadData(data)"></el-button>
+        
       </div>
     </div>
   </card>
@@ -16,6 +23,19 @@
   font-size: 14px;
   width: 360px;
   padding: 4px 0;
+  display: flex;
+}
+
+.content_box {
+  flex: 1;
+}
+
+.button_box {
+  width: 32px;
+}
+
+.buttons {
+  border: none;
 }
 
 .data_text {
@@ -53,6 +73,10 @@ import { defineComponent } from "vue";
 import { bridge } from '../event'
 import dayjs from 'dayjs';
 import { EVENT } from "@/constant";
+import {
+  Download,
+  CopyDocument
+} from '@element-plus/icons-vue'
 
 export default defineComponent({
   name: "RecvListComponent",
@@ -79,7 +103,9 @@ export default defineComponent({
 
     return {
       formatDate,
-      downloadData
+      downloadData,
+      Download,
+      CopyDocument
     }
   }
 })
